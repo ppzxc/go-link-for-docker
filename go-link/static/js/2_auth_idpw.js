58a9938cd57c3179a662ae7da7ac7b0900a166d6
JeongHa-Cho <cjh8487@naver.com>
2021-07-07 09:27:19 +0900

    2021-07-07 delete static test page

--- go-link/static/js/2_auth_idpw.js
+++
@@ -1,8 +0,0 @@
-let authLoginUserForm = document.getElementById("authLoginUserForm");
-let authLoginUserIdInput = document.getElementById("authLoginUserIdInput");
-let authLoginUserPwInput = document.getElementById("authLoginUserPwInput");
-
-authLoginUserForm.onsubmit = function () {
-    Status("<b>ID, PASSWORD 로그인 구현 안됨</b>");
-    return false;
-};
