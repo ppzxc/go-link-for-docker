18aecc33c579f940a6489a8e9ad98338184bcb6e
JeongHa-Cho <cjh8487@naver.com>
2021-07-19 12:51:51 +0900

    2021-07-19 commit

--- go-link/static/js/websocket.js
+++ go-link/static/js/websocket.js
@@ -12,8 +12,24 @@ let buttonRegister = document.getElementById("buttonRegister");
 let buttonIdPassword = document.getElementById("buttonIdPassword");
 let buttonAnonymous = document.getElementById("buttonAnonymous");
 let buttonToken = document.getElementById("buttonToken");
+let buttonRequestToken = document.getElementById("buttonRequestToken");
 let buttonRotary = document.getElementById("buttonRotary");
 let buttonLogout = document.getElementById("buttonLogout");
+let buttonCreateRoom = document.getElementById("buttonCreateRoom");
+let buttonJoinRoom = document.getElementById("buttonJoinRoom");
+let buttonLeaveRoom = document.getElementById("buttonLeaveRoom");
+let buttonInviteRoom = document.getElementById("buttonInviteRoom");
+let buttonTestLogin = document.getElementById("buttonTestLogin");
+
+let buttonMetaAllChatRoom = document.getElementById("buttonMetaAllChatRoom");
+let buttonMetaMyChatRoom = document.getElementById("buttonMetaMyChatRoom");
+let buttonMetaAllUser = document.getElementById("buttonMetaAllUser");
+let buttonMetaChatRoomUser = document.getElementById("buttonMetaChatRoomUser");
+let buttonMetaChatRoomMessage = document.getElementById("buttonMetaChatRoomMessage");
+
+let buttonMsgSend = document.getElementById("buttonMsgSend");
+let buttonMsgReceiveAck = document.getElementById("buttonMsgReceiveAck");
+let buttonMsgReadAck = document.getElementById("buttonMsgReadAck");
 
 let buttonSend = document.getElementById("buttonSend");
 
@@ -32,14 +48,141 @@ buttonSend.onclick = function () {
     websocketConnection.send(inputJson.value)
 }
 
+buttonMsgSend.onclick = function () {
+    let root = defaultJsonObject("msg")
+    root.msg.request.what = "message"
+    root.msg.request.how = "send"
+    root.msg.request.topic_id = 0
+    root.msg.request.message = ""
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMsgReadAck.onclick = function () {
+    let root = defaultJsonObject("msg")
+    root.msg.request.what = "message"
+    root.msg.request.how = "read"
+    root.msg.request.topic_id = 0
+    root.msg.request.sequence_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMsgReceiveAck.onclick = function () {
+    let root = defaultJsonObject("msg")
+    root.msg.request.what = "message"
+    root.msg.request.how = "ack"
+    root.msg.request.topic_id = 0
+    root.msg.request.sequence_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMetaAllChatRoom.onclick = function () {
+    let root = defaultJsonObject("meta")
+    root.meta.request.what = "topic"
+    root.meta.request.how = "select"
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMetaMyChatRoom.onclick = function () {
+    let root = defaultJsonObject("meta")
+    root.meta.request.what = "topic"
+    root.meta.request.how = "select"
+    root.meta.request.using = "me"
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMetaAllUser.onclick = function () {
+    let root = defaultJsonObject("meta")
+    root.meta.request.what = "user"
+    root.meta.request.how = "select"
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMetaChatRoomUser.onclick = function () {
+    let root = defaultJsonObject("meta")
+    root.meta.request.what = "user"
+    root.meta.request.how = "select"
+    root.meta.request.using = "topic"
+    root.meta.request.topic_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonMetaChatRoomMessage.onclick = function () {
+    let root = defaultJsonObject("meta")
+    root.meta.request.what = "message"
+    root.meta.request.how = "select"
+    root.meta.request.topic_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonCreateRoom.onclick = function () {
+    let root = defaultJsonObject("ctrl")
+    root.ctrl.request.what = "topic"
+    root.ctrl.request.how = "create"
+    root.ctrl.request.who = "alone"
+    root.ctrl.request.private = false
+    root.ctrl.request.topic_name = ""
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonJoinRoom.onclick = function () {
+    let root = defaultJsonObject("ctrl")
+    root.ctrl.request.what = "topic"
+    root.ctrl.request.how = "join"
+    root.ctrl.request.topic_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonLeaveRoom.onclick = function () {
+    let root = defaultJsonObject("ctrl")
+    root.ctrl.request.what = "topic"
+    root.ctrl.request.how = "leave"
+    root.ctrl.request.topic_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonInviteRoom.onclick = function () {
+    let root = defaultJsonObject("ctrl")
+    root.ctrl.request.what = "topic"
+    root.ctrl.request.how = "invite"
+    root.ctrl.request.user_id = 0
+    root.ctrl.request.topic_id = 0
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
 buttonRegister.onclick = function () {
     let root = defaultJsonObject("auth")
     root.auth.request.what = "user"
     root.auth.request.how = "register"
     root.auth.request.user = {}
-    root.auth.request.user.name = ""
-    root.auth.request.user.email = ""
-    root.auth.request.user.password = ""
+    root.auth.request.user.auth = {}
+    root.auth.request.user.auth.name = ""
+    root.auth.request.user.auth.email = ""
+    root.auth.request.user.auth.password = ""
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
+buttonTestLogin.onclick = function () {
+    let root = defaultJsonObject("auth")
+    root.auth.request.what = "session"
+    root.auth.request.how = "login"
+    root.auth.request.using = "id"
+    root.auth.request.user = {}
+    root.auth.request.user.auth = {}
+    root.auth.request.user.auth.email = "asdf@asdf"
+    root.auth.request.user.auth.password = "asdfasdf"
 
     inputJson.value = JSON.stringify(root, null, 4)
 }
@@ -50,8 +193,9 @@ buttonIdPassword.onclick = function () {
     root.auth.request.how = "login"
     root.auth.request.using = "id"
     root.auth.request.user = {}
-    root.auth.request.user.email = ""
-    root.auth.request.user.password = ""
+    root.auth.request.user.auth = {}
+    root.auth.request.user.auth.email = ""
+    root.auth.request.user.auth.password = ""
 
     inputJson.value = JSON.stringify(root, null, 4)
 }
@@ -75,6 +219,14 @@ buttonRotary.onclick = function () {
     inputJson.value = JSON.stringify(root, null, 4)
 }
 
+buttonRequestToken.onclick = function () {
+    let root = defaultJsonObject("auth")
+    root.auth.request.what = "jwt"
+    root.auth.request.how = "select"
+
+    inputJson.value = JSON.stringify(root, null, 4)
+}
+
 buttonToken.onclick = function () {
     let root = defaultJsonObject("auth")
     root.auth.request.what = "session"
@@ -94,9 +246,10 @@ buttonLogout.onclick = function () {
 }
 
 function Log(msg) {
-    let item = document.createElement("div");
+    let item = document.createElement("li");
     logMsgKey = logMsgKey + 1;
-    item.id = "logging " + logMsgKey
+    // item.id = "logging " + logMsgKey
+    item.key = logMsgKey
     item.innerHTML = msg;
 
     let doScroll = logDiv.scrollTop > logDiv.scrollHeight - logDiv.clientHeight - 1;
@@ -107,24 +260,64 @@ function Log(msg) {
     }
 }
 
+function AddResult(msg) {
+    let doScroll = receiveJson.scrollTop > receiveJson.scrollHeight - receiveJson.clientHeight - 1;
+    receiveJson.value = receiveJson.value + "====================================\r\n"
+    receiveJson.value = receiveJson.value + msg
+    if (doScroll) {
+        receiveJson.scrollTop = receiveJson.scrollHeight - receiveJson.clientHeight;
+    }
+}
+
 function parseWebsocketReadMessage(msg) {
     Log("RECV " + msg)
     let recv = JSON.parse(msg)
     if (recv.ctrl !== undefined) {
-        receiveJson.value = JSON.stringify(recv, null, 4)
+        AddResult(JSON.stringify(recv, null, 4))
+        // receiveJson.value = receiveJson.value + JSON.stringify(recv, null, 4)
     } else if (recv.auth !== undefined) {
-        receiveJson.value = JSON.stringify(recv, null, 4)
+        AddResult(JSON.stringify(recv, null, 4))
+        // receiveJson.value = receiveJson.value + JSON.stringify(recv, null, 4)
     } else if (recv.msg !== undefined) {
-        receiveJson.value = JSON.stringify(recv, null, 4)
+        AddResult(JSON.stringify(recv, null, 4))
+        // receiveJson.value = receiveJson.value + JSON.stringify(recv, null, 4)
+        if (recv.result.status_code === 200) {
+            let recvAck = {}
+            recvAck.msg = {}
+            recvAck.msg.uuid = uuidv4()
+            recvAck.msg.request = {}
+            recvAck.msg.request.what = "message"
+            recvAck.msg.request.how = "ack"
+            recvAck.msg.request.topic_id = recv.msg.request.topic_id
+            recvAck.msg.request.sequence_id = recv.msg.response.message.sequence_id
+            let recvAckJson = JSON.stringify(recvAck)
+            Log("SEND " + recvAckJson)
+            websocketConnection.send(recvAckJson)
+
+            let readAck = {}
+            readAck.msg = {}
+            readAck.msg.uuid = uuidv4()
+            readAck.msg.request = {}
+            readAck.msg.request.what = "message"
+            readAck.msg.request.how = "read"
+            readAck.msg.request.topic_id = recv.msg.request.topic_id
+            readAck.msg.request.sequence_id = recv.msg.response.message.sequence_id
+            let readAckJson = JSON.stringify(readAck)
+            Log("SEND " + readAckJson)
+            websocketConnection.send(readAckJson)
+        }
     } else if (recv.meta !== undefined) {
-        receiveJson.value = JSON.stringify(recv, null, 4)
+        AddResult(JSON.stringify(recv, null, 4))
+        // receiveJson.value = receiveJson.value + JSON.stringify(recv, null, 4)
     } else if (recv.ack !== undefined) {
         if (recv.result.status_code !== 200) {
-            receiveJson.value = JSON.stringify(recv, null, 4)
+            AddResult(JSON.stringify(recv, null, 4))
+            // receiveJson.value = receiveJson.value + JSON.stringify(recv, null, 4)
         }
     } else {
         Log("json root object가 없음 ctrl, msg, auth, meta")
-        receiveJson.value = JSON.stringify(recv, null, 4)
+        AddResult(JSON.stringify(recv, null, 4))
+        // receiveJson.value = receiveJson.value + JSON.stringify(recv, null, 4)
     }
 }
 
@@ -149,6 +342,10 @@ if (window["WebSocket"]) {
         connection_status.innerText = "CONNECTION STATUS : ERROR"
         Log("[error] ${error.message}");
     };
+
+    // websocketConnection.on("ping", function () {
+    //     Log("receive ping")
+    // })
 
     websocketConnection.onmessage = function (evt) {
         connection_status.innerText = "CONNECTION STATUS : OPEN"
